8c412c9b0556fe7b7cf3912c3ae5e491e4af370b
melyasanya <werbolom@gmail.com>
2023-09-21 11:19:37 +0300

    testing

--- NativeTestProject/App.js
+++ NativeTestProject/App.js
@@ -1,11 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require("./images/PhotoBG.png")}
+        style={styles.bgImage}
+      >
+        <View style={styles.registrationContainer}>
+          <Text style={styles.registrationText}>Реєстрація</Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +19,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bgImage: {
+    flex: 1,
+    // height: "100%",
+    // width: "100%",
+  },
+  registrationContainer: {
+    backgroundColor: "#fff",
+    height: 549,
+    width: "100%",
+    marginTop: "auto",
+    borderTopLeftRadius: "25%",
+    borderTopRightRadius: "25%",
+  },
+  registrationText: {
+    textAlign: "center",
+    marginTop: 92,
   },
 });
